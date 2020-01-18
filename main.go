a200d4233d5bcf47fae0eb428fc9a89565dc6401
Andrew Qu <qua@rpi.edu>
2020-01-18 13:57:05 -0500

    created initial node and edge classes for NFA

--- main.go
+++ main.go
@@ -2,65 +2,30 @@ package main
 
 import "fmt"
 
-// Parse range qualifiers first
-// RangeQuantifier ::= "{" Number ( "," Number? )? "}"
-/*
-	a{n} – matches “a” exactly n times
-	a{n,} – matches “a” at least n times
-	a{n,m} – matches “a” from n to m times
-*/
-func parseRangeQualifier(s string) {
-
-	// parse the initial opening bracket
-	testing := parseString("{", s)
-	fmt.Println(testing)
+type Edge struct {
+	From  Node
+	To    Node
+	Value string
 }
 
-func parseString(prefix string, full string) string {
-	if full[0:len(prefix)] == prefix {
-
-		fmt.Println("matched")
-
-		toReturn := full[len(prefix):]
-		return toReturn
-	}
-
-	return "nothing"
-
+type Node struct {
+	accepted bool
+	next     []Edge
 }
 
-// check equality of a single character
-func matchOne(pattern string, text string) bool {
+func createMachine(expression string) {
 
-	// all text matches empty pattern
-	if pattern == "" {
-		return true
-	}
-	if text == "" {
-		return false
-	}
-	if pattern == "." {
-		return true
-	}
-	return pattern == text
-
-}
-
-func match(pattern string, text string) bool {
-
-	if pattern == "" {
-		return true
-	} else {
-
-		return matchOne(string(pattern[0]), string(text[0])) && match(pattern[1:], text[1:])
+	for _, value := range expression {
+		fmt.Println(value)
 
 	}
 
 }
+
 func main() {
 	fmt.Println("vim-go")
 
-	//regex_expression := "{1,2}"
-	fmt.Println(match("abcd", "abcd"))
+	createMachine("abcd")
+
 	//parseRangeQualifier(regex_expression)
 }
